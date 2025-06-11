39310e69d39db04da9bad8942e705679f4426993
Paul Menard <63054997+PaulMNRD@users.noreply.github.com>
2025-06-11 12:12:10 +0200

    Update dashboard.sh

--- scripts/dashboard.sh
+++ scripts/dashboard.sh
@@ -2,7 +2,7 @@
 
 info "°°° Installation du dashboard °°°"
 echo "Installation des dépendances..."
-sudo apt install -y python3.11-venv python3-pip python3-tk python3-psutil python3-pytz
+sudo apt install -y python3.11-venv python3-pip python3-tk python3-psutil
 
 echo "Installation du dashboard en tant qu'exécutable..."
 sudo cp -r dashboard /usr/local/lib/dashboard/
