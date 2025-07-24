59385b49237ae813748c693ba73a094a557e8825
paul <paulmenard07104@gmail.com>
2025-07-24 14:28:36 +0200

    Pam time fix

--- scripts/pam.sh
+++ scripts/pam.sh
@@ -5,16 +5,14 @@ echo "Installation des dependances..."
 sudo apt install -y libpam-pwquality libpam-modules libpam-modules-bin libpam-runtime
 
 echo "Configuration des politiques de sécurité..."
-# disponibilité du login
-sudo cp configs/pam/time.conf /etc/security/time.conf
 
-# politique de login
-sudo cp configs/pam/common-auth /etc/pam.d/common-auth
-
-# politique des mots de passe
-sudo cp configs/pam/common-password /etc/pam.d/common-password
+# configuration pam
+sudo cp configs/pam/{common-auth,common-password,login} /etc/pam.d/
 
 # expiration des mots de passe
 sudo cp configs/pam/login.defs /etc/login.defs
+
+# disponibilité du login
+sudo cp configs/pam/time.conf /etc/security/time.conf
 
 success "Configuration de PAM terminée avec succès"
